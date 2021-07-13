907eb0502e9e72562dc2e556ccf2277d55f53f90
lightsing <light.tsing@gmail.com>
2021-07-13 17:14:14 +0800

    add POSEIDON_HASHER

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use once_cell::sync::Lazy;
+
 pub mod serde;
 pub mod types;
 
@@ -11,3 +13,6 @@ pub use num_bigint;
 pub use num_traits;
 pub use poseidon_rs;
 pub use rust_decimal;
+
+/// [`poseidon_rs::Poseidon`] global
+pub static POSEIDON_HASHER: Lazy<poseidon_rs::Poseidon> = Lazy::new(|| poseidon_rs::Poseidon::new());
