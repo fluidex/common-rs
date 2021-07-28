767a2f92b90e43d09aa01d089aea0fc09903f779
HAOYUatHZ <haoyu@protonmail.com>
2021-07-28 15:15:23 +0800

    fix

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -4,8 +4,11 @@ cfg_if::cfg_if! {
         pub use migrator::MIGRATOR;
     }
 }
-
-pub type DbType = sqlx::Postgres;
-pub type ConnectionType = sqlx::postgres::PgConnection;
-pub type PoolOptions = sqlx::postgres::PgPoolOptions;
-pub type DBErrType = sqlx::Error;
+cfg_if::cfg_if! {
+    if #[cfg(any(feature = "db"))] {
+        pub type DbType = sqlx::Postgres;
+        pub type ConnectionType = sqlx::postgres::PgConnection;
+        pub type PoolOptions = sqlx::postgres::PgPoolOptions;
+        pub type DBErrType = sqlx::Error;
+    }
+}
