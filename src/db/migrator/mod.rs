f146c34d90b7e136b51c2f26a93f8e8aaec244bb
Steven Gu <asongala@163.com>
2021-07-28 07:02:33 +0000

    Moves type DbType, ConnectionType, PoolOptions and DBErrType from mod `migrator` to `db`.

--- src/db/migrator/mod.rs
+++ src/db/migrator/mod.rs
@@ -1,11 +1,6 @@
 cfg_if::cfg_if! {
     if #[cfg(feature = "rollup-state-db")] {
         mod rollup_state_migrator;
-
         pub use rollup_state_migrator::MIGRATOR;
-        pub use rollup_state_migrator::DbType;
-        pub use rollup_state_migrator::ConnectionType;
-        pub use rollup_state_migrator::PoolOptions;
-        pub use rollup_state_migrator::DBErrType;
     }
 }
--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,11 +1,11 @@
 cfg_if::cfg_if! {
     if #[cfg(any(feature = "rollup-state-db"))] {
         mod migrator;
-
         pub use migrator::MIGRATOR;
-        pub use migrator::DbType;
-        pub use migrator::ConnectionType;
-        pub use migrator::PoolOptions;
-        pub use migrator::DBErrType;
+
+        pub type DbType = sqlx::Postgres;
+        pub type ConnectionType = sqlx::postgres::PgConnection;
+        pub type PoolOptions = sqlx::postgres::PgPoolOptions;
+        pub type DBErrType = sqlx::Error;
     }
 }
