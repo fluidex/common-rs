ab52f78f50c4cf985443c4a0b6dd25187e6f8d59
HAOYUatHZ <haoyu@protonmail.com>
2021-07-28 15:07:09 +0800

    fix

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -2,10 +2,10 @@ cfg_if::cfg_if! {
     if #[cfg(any(feature = "rollup-state-db"))] {
         mod migrator;
         pub use migrator::MIGRATOR;
-
-        pub type DbType = sqlx::Postgres;
-        pub type ConnectionType = sqlx::postgres::PgConnection;
-        pub type PoolOptions = sqlx::postgres::PgPoolOptions;
-        pub type DBErrType = sqlx::Error;
     }
 }
+
+pub type DbType = sqlx::Postgres;
+pub type ConnectionType = sqlx::postgres::PgConnection;
+pub type PoolOptions = sqlx::postgres::PgPoolOptions;
+pub type DBErrType = sqlx::Error;
