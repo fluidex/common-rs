2c3a1e68d6c6e68c56e9eb00196feb833b335b9b
HAOYUatHZ <haoyu@protonmail.com>
2021-08-06 20:39:12 +0800

    rename L2Block::witness to L2Block::detail

--- migrations/rollup_state/20210626034404_l2block.sql
+++ migrations/rollup_state/20210626034404_l2block.sql
@@ -1,6 +1,6 @@
 CREATE TABLE l2block (
     block_id BIGINT PRIMARY KEY,
     new_root VARCHAR(256) NOT NULL,
-    witness jsonb NOT NULL,
+    detail jsonb NOT NULL,
     created_time TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
