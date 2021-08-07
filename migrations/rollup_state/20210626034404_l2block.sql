7bac690daae82cbd7020bccf41a53e5233a9b764
HAOYUatHZ <haoyu@protonmail.com>
2021-08-07 12:55:26 +0800

    add TODOS

--- migrations/rollup_state/20210626034404_l2block.sql
+++ migrations/rollup_state/20210626034404_l2block.sql
@@ -1,6 +1,7 @@
 CREATE TABLE l2block (
     block_id BIGINT PRIMARY KEY,
     new_root VARCHAR(256) NOT NULL,
+    -- block_status: unsubmitted, submitted, confirmed
     detail jsonb NOT NULL,
     created_time TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
--- src/db/models/rollup_state/l2_block.rs
+++ src/db/models/rollup_state/l2_block.rs
@@ -5,6 +5,7 @@ use serde::Serialize;
 pub struct L2Block {
     pub block_id: i64, // TODO: keep this consistent with the smart contract
     pub new_root: String,
+    // block_status: unsubmitted, submitted, confirmed
     pub detail: serde_json::Value,
     pub created_time: TimestampDbType,
 }
