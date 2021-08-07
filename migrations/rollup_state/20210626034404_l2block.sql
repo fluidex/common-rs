12344adb91eb1e47a4b3ff3cb7fa73efd6c8c9e3
HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>
2021-08-07 12:57:09 +0800

    update TODOs (#15)
    * add TODOS
    
    * update

--- migrations/rollup_state/20210626034404_l2block.sql
+++ migrations/rollup_state/20210626034404_l2block.sql
@@ -1,6 +1,8 @@
 CREATE TABLE l2block (
     block_id BIGINT PRIMARY KEY,
     new_root VARCHAR(256) NOT NULL,
+    -- TODO: block_status: unsubmitted, submitted, confirmed
+    -- TODO: tx_hash
     detail jsonb NOT NULL,
     created_time TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
