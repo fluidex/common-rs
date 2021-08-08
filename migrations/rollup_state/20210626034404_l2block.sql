7184f197f34376b079719f832368b12aa077b5d7
HAOYUatHZ <haoyu@protonmail.com>
2021-08-09 05:55:30 +0800

    db(rollup_state model): add block_status to block

--- migrations/rollup_state/20210626034404_l2block.sql
+++ migrations/rollup_state/20210626034404_l2block.sql
@@ -1,7 +1,9 @@
+CREATE TYPE block_status AS ENUM('uncommited', 'commited', 'submitted', 'confirmed');
+
 CREATE TABLE l2block (
     block_id BIGINT PRIMARY KEY,
     new_root VARCHAR(256) NOT NULL,
-    -- TODO: block_status: unsubmitted, submitted, confirmed
+    status block_status NOT NULL DEFAULT 'uncommited',
     -- TODO: tx_hash
     detail jsonb NOT NULL,
     created_time TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP
--- src/db/models/rollup_state/l2_block.rs
+++ src/db/models/rollup_state/l2_block.rs
@@ -1,11 +1,20 @@
 use crate::db::TimestampDbType;
 use serde::Serialize;
 
+#[derive(sqlx::Type, Serialize, Debug, Clone)]
+#[sqlx(type_name = "block_status", rename_all = "snake_case")]
+pub enum BlockStatus {
+    Uncommited,
+    Commited,
+    Submitted,
+    Confirmed,
+}
+
 #[derive(sqlx::FromRow, Serialize, Debug, Clone)]
 pub struct L2Block {
     pub block_id: i64, // TODO: keep this consistent with the smart contract
     pub new_root: String,
-    // TODO: block_status: unsubmitted, submitted, confirmed
+    pub status: BlockStatus,
     // TODO: tx_hash
     pub detail: serde_json::Value,
     pub created_time: TimestampDbType,
