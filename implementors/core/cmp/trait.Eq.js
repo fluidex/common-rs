34f267767ac8edf35b1e4314b63f57ad0568a4b3
HAOYUatHZ <HAOYUatHZ@users.noreply.github.com>
2021-09-17 05:50:32 +0000

    deploy: 985a813f6c8a6f7a835043da11150ddff02ae4ed

--- implementors/core/cmp/trait.Eq.js
+++ implementors/core/cmp/trait.Eq.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["fluidex_common"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.54.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"fluidex_common/helper/enum.Order.html\" title=\"enum fluidex_common::helper::Order\">Order</a>","synthetic":false,"types":["fluidex_common::helper::Order"]}];
+implementors["fluidex_common"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.55.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"fluidex_common/helper/enum.Order.html\" title=\"enum fluidex_common::helper::Order\">Order</a>","synthetic":false,"types":["fluidex_common::helper::Order"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["fluidex_common"] = {"name":"","dirs":[{"name":"db","dirs":[{"name":"models","files":["mod.rs"]}],"files":["mod.rs"]},{"name":"message","files":["mod.rs"]},{"name":"types","files":["decimal.rs","float864.rs","mod.rs","pubkey.rs","signature.rs"]},{"name":"utils","files":["mod.rs","timeutil.rs"]}],"files":["helper.rs","lib.rs","serde.rs"]};
+sourcesIndex["fluidex_common"] = {"name":"","dirs":[{"name":"db","dirs":[{"name":"models","files":["mod.rs"]}],"files":["mod.rs"]},{"name":"message","files":["mod.rs"]},{"name":"types","files":["decimal.rs","float864.rs","floats.rs","mod.rs","pubkey.rs","signature.rs"]},{"name":"utils","files":["mod.rs","timeutil.rs"]}],"files":["helper.rs","lib.rs","serde.rs"]};
 createSourceSidebar();
