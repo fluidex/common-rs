34adf3639428a0a65360cfaaf24f3346eec92350
Steven Gu <asongala@163.com>
2021-11-15 11:28:55 +0000

    Remove `CircuitType`.

--- src/db/models/rollup_state/task.rs
+++ src/db/models/rollup_state/task.rs
@@ -1,5 +1,5 @@
 use crate::db::TimestampDbType;
-use serde::{Deserialize, Serialize};
+use serde::Serialize;
 
 #[derive(sqlx::Type, Serialize, Debug, Clone)]
 #[sqlx(type_name = "task_status", rename_all = "snake_case")]
@@ -9,16 +9,10 @@ pub enum TaskStatus {
     Proved,
 }
 
-#[derive(sqlx::Type, Serialize, Deserialize, Debug, Clone, Copy, PartialEq)]
-#[sqlx(type_name = "varchar", rename_all = "lowercase")]
-pub enum CircuitType {
-    BLOCK,
-}
-
 #[derive(sqlx::FromRow, Serialize, Debug, Clone)]
 pub struct Task {
     pub task_id: String,
-    pub circuit: CircuitType,
+    pub circuit: String,
     pub block_id: i64,
     pub input: serde_json::Value,
     pub output: Option<serde_json::Value>,
